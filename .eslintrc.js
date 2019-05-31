279a177a5b151cf0fc2b5e0de24f928f9593e20c
Zach Hardesty <zachhardesty7@gmail.com>
2019-05-31 13:15:14 -0500

    update dev tool dot files

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:promise/recommended',
+    'airbnb',
+    'standard'
+  ],
+  plugins: [
+    'react-hooks'
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  globals: {
+    fetch: true
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-raw-text': 'off',
+    'react/prop-types': [
+      'warn',
+      {
+        skipUndeclared: true
+      }
+    ],
+    'react/require-default-props': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: [
+          '.js',
+          '.jsx'
+        ]
+      }
+    ],
+    'react/jsx-indent': [
+      'warn',
+      2,
+      {
+        checkAttributes: true,
+        indentLogicalExpressions: true
+      }
+    ],
+    'jsx-quotes': [
+      'error',
+      'prefer-single'
+    ],
+    'standard/array-bracket-even-spacing': 'off',
+    'standard/computed-property-even-spacing': 'off',
+    'standard/object-curly-even-spacing': 'off',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'import/namespace': 'error',
+    'sort-imports': [
+      'warn',
+      {
+        ignoreDeclarationSort: true
+      }
+    ],
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ]
+  }
+}
