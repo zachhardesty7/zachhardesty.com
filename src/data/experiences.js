e8ee4c9fd5d52d4047ef2d33d73574ef8240e4da
Zach <hello@zachhardesty.com>
2020-01-06 16:41:15 -0600

    chore: add IBM job info

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,13 +1,23 @@
 export default [
   {
+    employer: "International Business Machines (IBM)",
+    position: "Front End Software Engineer",
+    location: "Austin, TX",
+    date: "June 2019 – Present",
+    bullets: [
+      "Released public React (Typescript) IBM Cloud Identity user registration starter app",
+      "Built React IBM Cloud Identity FIDO2 password-less authentication demo",
+      "Contributed React Grid component to IBM’s open-source Carbon Design System"
+    ]
+  },
+  {
     employer: "Ticom Geomatics, Inc.",
     position: "Front End Software Engineer",
     location: "Austin, TX",
     date: "May 2018 – August 2018",
     bullets: [
       "Architected custom reactive MapboxGL “plugin” interface to abstract lifecycle handling",
-      "Redesigned mapping element of flagship product with a focus on modern UI/UX",
-      "Evaluated mapping frameworks for performance, usability, and extensibility"
+      "Redesigned mapping feature of flagship product with a focus on modern UI/UX"
     ]
   },
   {
@@ -16,12 +26,9 @@ export default [
     location: "Dallas, TX",
     date: "May 2015 - May 2018",
     bullets: [
-      "Independently managed time and resources when given high-level project assignments",
       "Boosted sales by creating WooCommerce-embeddable iFrame widget using Bootstrap 4",
-      "Developed simple React.js app to enhance UX by generating an iFrame embed snippet",
-      "Implemented PHP-customized WooCommerce system improve sales conversion rate",
-      "Overhauled and redesigned company website with heavily personalized WordPress theme",
-      "Optimized company website for performance and Search Engine Optimization (SEO)"
+      "Developed basic React.js app to enhance UX by generating an iFrame embed snippet",
+      "Overhauled, optimized (SEO), and redesigned website with personalized WordPress theme"
     ]
   }
 ];
