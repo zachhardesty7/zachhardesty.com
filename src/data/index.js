9f3b37bbb20c09e7977523855595084dbfea8855
Zach Hardesty <hello@zachhardesty.com>
2021-03-26 00:27:19 -0500

    refactor: extract about content

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,9 @@
 import apps from "./apps";
+import about from "./about";
 import data from "./data";
 import experiences from "./experiences";
 import icons from "./icons";
 import skills from "./skills";
 import websites from "./websites";
 
-export { apps, data, experiences, icons, skills, websites };
+export { apps, about, data, experiences, icons, skills, websites };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Experience, Footer, Hero, Icons, Portfolio } from "../components";
-  import { data, icons, skills } from "../data";
+  import { about, data, icons, skills } from "../data";
 </script>
 
 <svelte:head>
@@ -13,15 +13,8 @@
     <h2>About Me</h2>
     <div><img class="portrait" alt="portrait" src="/portrait.jpg" /></div>
 
-    <p>
-      {`First, allow me to tell you my story. I am now enrolled in the University of Texas at Austin's computer science degree plan but my journey began much earlier. From a young age, understanding the “why” and the “how” of everything from common items to modern marvels fascinated me. Naturally, once I received my parents' old computer I began tinkering and familiarizing myself. Learning to program daunted me but I hesitantly began exploring the basics of HTML, CSS, and JavaScript in my spare time. After getting a job with my neighbor, I started tinkering around in WordPress. There, I uncovered my fascination with web development and the rest is history.`}
-    </p>
-    <p>
-      {`I'm currently a front end website developer, but what does the future entail for
-      me? Cryptocurrency/blockchain, machine learning, and mobile app development all
-      pique my interests, but I stay open to all possibilities. Please do not hesitate
-      to contact me with opportunities or inquiries and connect with me on LinkedIn!`}
-    </p>
+    <p>{about.background}</p>
+    <p>{about.plans}</p>
     <div class="skills">
       <b>Proficient with:</b>
       <p>{skills.proficient.join(", ")}</p>
