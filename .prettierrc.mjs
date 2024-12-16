77c9dd932b936af58a2b8ff29f3644ec9b0923ae
Zach Hardesty <hello@zachhardesty.com>
2024-12-15 22:33:19 -0600

    chore(prettier): upd config

---
+++ .prettierrc.mjs
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 88, // <- 80
+  // rarely intended, insert spaces if needed manually (e.g. {' '}), see https://prettier.io/blog/2018/11/07/1.15.0#whitespace-sensitive-formatting
+  htmlWhitespaceSensitivity: "ignore", // <- css
+};
