bfeaa166c4740b635edb0438fd3745e4fc62aaff
Zach Hardesty <hello@zachhardesty.com>
2025-06-01 19:40:10 -0500

    chore(exp): clean up

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -6,8 +6,8 @@ export default [
     date: "September 2020 – Present",
     bullets: [
       "Rewrote 7k+ lines of ancient AngularJS code to Typescript React from scratch",
-      "Mentored and helped bring four newly-hired employees on the team up to speed",
-      "Lead effort to update three-year-old Angular & Carbon Components React UI packages",
+      "Mentored and helped bring four newly-hired employees on my team up to speed",
+      "Led effort to update three-year-old Angular & Carbon Components React UI packages",
     ],
   },
   {
@@ -16,9 +16,8 @@ export default [
     location: "Austin, TX",
     date: "June 2019 – August 2020",
     bullets: [
-      "Released public React (Typescript) IBM Cloud Identity user registration starter app",
-      "Built React IBM Cloud Identity FIDO2 password-less authentication demo",
-      "Contributed React Grid component to IBM’s open-source Carbon Design System",
+      "Built React (Typescript) IBM Cloud Identity FIDO2 password-less authentication POC",
+      "Contributed React grid component to IBM’s open-source Carbon Design System",
     ],
   },
   {
@@ -35,10 +34,10 @@ export default [
     employer: "Mark Porter, Inc.",
     position: "Front End Developer",
     location: "Dallas, TX",
-    date: "May 2015 - May 2018",
+    date: "May 2015 – May 2018",
     bullets: [
       "Boosted sales by creating WooCommerce-embeddable iFrame widget using Bootstrap 4",
-      "Developed basic React.js app to enhance UX by generating an iFrame embed snippet",
+      "Developed basic React app to enhance UX by generating an iFrame embed snippet",
       "Overhauled, optimized (SEO), and redesigned website with personalized WordPress theme",
     ],
   },
