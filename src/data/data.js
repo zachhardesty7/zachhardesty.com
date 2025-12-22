e1523d6a13115f413dae1eede4595f9b4a97f749
Zach Hardesty <hello@zachhardesty.com>
2025-12-22 17:41:58 -0600

    feat(data): reorder projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,11 @@
 export default [
   {
-    title: "My Personal Portfolio Website",
+    title: "Tampermonkey / Greasemonkey Scripts Collection",
     description:
-      "This is the website you’re looking at right now! It’s built with Svelte and their SSR/SSG framework Sapper to statically generate the output HTML, JS, and CSS. I wired the repo up to Netlify to allow me to easily add, remove, and update projects by simply modifying a JSON file and committing it. This project implements CSS styling inspired by Material Design standards and semi-dynamically sized images for quick loading. This website dramatically improves on my first website back when I thought DreamWeaver was a good idea.",
-    image: "website.png",
-    link: "https://zachhardesty.com",
-    github: "https://github.com/zachhardesty7/zachhardesty.com",
-    skills: ["Svelte", "Sapper", "JavaScript", "CSS", "CSS Grid", "HTML5", "Webpack"],
-    case: {
-      overview: [],
-      context: [],
-      challenge: [],
-      objectives: [],
-      process: [],
-      insight: [],
-      solution: [],
-      results: [],
-    },
+      "A GitHub repo of a handful of different script files that Tampermonkey can inject into the relevant websites. Certain websites often seem to either overlook something, or a simple change could really improve their functionality. I took it upon myself to add these features in the most seamless way possible.",
+    link: "https://openuserjs.org/users/zachhardesty7/scripts",
+    github: "https://github.com/zachhardesty7/tamper-monkey-scripts-collection",
+    skills: ["JavaScript", "Tampermonkey", "Greasemonkey", "Userscript", "Extension"],
   },
   {
     title: "Missive GCal Export Widget",
@@ -49,20 +38,31 @@ export default [
     ],
   },
   {
+    title: "My Personal Portfolio Website",
+    description:
+      "This is the website you’re looking at right now! It’s built with Svelte and their SSR/SSG framework Sapper to statically generate the output HTML, JS, and CSS. I wired the repo up to Netlify to allow me to easily add, remove, and update projects by simply modifying a JSON file and committing it. This project implements CSS styling inspired by Material Design standards and semi-dynamically sized images for quick loading. This website dramatically improves on my first website back when I thought DreamWeaver was a good idea.",
+    image: "website.png",
+    link: "https://zachhardesty.com",
+    github: "https://github.com/zachhardesty7/zachhardesty.com",
+    skills: ["Svelte", "Sapper", "JavaScript", "CSS", "CSS Grid", "HTML5", "Webpack"],
+    case: {
+      overview: [],
+      context: [],
+      challenge: [],
+      objectives: [],
+      process: [],
+      insight: [],
+      solution: [],
+      results: [],
+    },
+  },
+  {
     title: "Semantic Styled UI",
     description:
       "A UI component library for React that styles content based on Semantic UI, but improves the default styles with Styled Components. It allows for easy plug-and-play in projects looking to take advantage of Styled Components, without dealing with the hassle of integrating Semantic UI styles manually. It also includes some useful components and pre-built components for rapid web app development.",
     link: "",
     github: "https://github.com/zachhardesty7/semantic-styled-ui",
     skills: ["JavaScript", "React", "SemanticUI", "Styled Components", "CSS-in-JS"],
-  },
-  {
-    title: "Tampermonkey / Greasemonkey Scripts Collection",
-    description:
-      "A GitHub repo of a handful of different script files that Tampermonkey can inject into the relevant websites. Certain websites often seem to either overlook something, or a simple change could really improve their functionality. I took it upon myself to add these features in the most seamless way possible.",
-    link: "https://openuserjs.org/users/zachhardesty7/scripts",
-    github: "https://github.com/zachhardesty7/tamper-monkey-scripts-collection",
-    skills: ["JavaScript", "Tampermonkey", "Greasemonkey", "Userscript", "Extension"],
   },
   {
     title: "GroupMe Notifier",
