df16428bcddd47964eda54b590e062c4188a99b1
Zach Hardesty <hello@zachhardesty.com>
2025-12-22 17:34:14 -0600

    feat(experience): upd from new resume

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -5,9 +5,15 @@ export default [
     location: "Austin, TX",
     date: "September 2020 – Present",
     bullets: [
-      "Rewrote 7k+ lines of ancient AngularJS code to Typescript React from scratch",
-      "Mentored and helped bring four newly-hired employees on my team up to speed",
-      "Led effort to update three-year-old Angular & Carbon Components React UI packages",
+      "Modernized IBM Verify’s front-end stack (TypeScript 5, React 18, Webpack 5), strengthening type safety & cutting bundle size by 91.7%, build times by 63%, and deployment times by 36%",
+      "Resolved critical security vulnerabilities (ReDoS, XSS), reinforcing system integrity & user trust",
+      "Architected JSON i18n system with code splitting for faster, dynamic translation loading",
+      "Overhauled Verify’s in-house authentication solution to use OAuth2 & innovative DPoP tokens",
+      "Led ESLint migration and added automated checks, raising product’s code quality standards",
+      "Designed & implemented robust, reusable React components adopted across Verify teams",
+      "Cohosted weekly code reviews, fostering culture of continuous learning & technical excellence",
+      "Mentored junior developers for 3.5+ years, providing guidance and promoting team growth",
+      "Published 5 disclosures on inventive technical solutions to everyday problems on IP.com",
     ],
   },
   {
@@ -16,18 +22,18 @@ export default [
     location: "Austin, TX",
     date: "June 2019 – August 2020",
     bullets: [
-      "Built React (Typescript) IBM Cloud Identity FIDO2 password-less authentication POC",
-      "Contributed React grid component to IBM’s open-source Carbon Design System",
+      "Built TypeScript React FIDO2 password-less authentication POC for IBM Verify IAM",
+      "Contributed React grid component to IBM’s open-source Carbon Design System UI library",
     ],
   },
   {
     employer: "Ticom Geomatics, Inc.",
-    position: "Front End Software Engineer",
+    position: "Front End Software Developer Intern",
     location: "Austin, TX",
     date: "May 2018 – August 2018",
     bullets: [
       "Architected custom reactive MapboxGL “plugin” interface to abstract lifecycle handling",
-      "Redesigned mapping feature of flagship product with a focus on modern UI/UX",
+      "Redesigned mapping feature of flagship React product with a focus on modern UI/UX",
     ],
   },
   {
@@ -36,8 +42,7 @@ export default [
     location: "Dallas, TX",
     date: "May 2015 – May 2018",
     bullets: [
-      "Boosted sales by creating WooCommerce-embeddable iFrame widget using Bootstrap 4",
-      "Developed basic React app to enhance UX by generating an iFrame embed snippet",
+      "Developed React app to generate WooCommerce embed code for custom Bootstrap 4 widget",
       "Overhauled, optimized (SEO), and redesigned website with personalized WordPress theme",
     ],
   },
